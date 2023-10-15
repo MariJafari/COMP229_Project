9653afde07f10a6e9d9e20e28dfec6dd44702d26
karendelacruz <kdelac14@my.centennialcollege.ca>
2023-10-14 20:26:37 -0400

    Added delete-confirm function

--- .history/Server/Config/db_20231013160433.ts
+++
@@ -1,4 +0,0 @@
-export const LocalURI = "mongodb://127.0.0.1/media";
-export const RemoteURI="mongodb+srv://kdelac14:SuSYJcSEZwL9OSx4@cluster0.dln0xzp.mongodb.net/media?retryWrites=true&w=majority";
-export const HostName = "MongoDB Atlas";
-export const Secret = "someSecret";
--- .history/Server/Config/db_20231013213605.ts
+++
@@ -1,5 +0,0 @@
-//export const LocalURI = "mongodb://127.0.0.1/media";
-export const LocalURI = "mongodb://127.0.0.1/Marketplace";
-export const RemoteURI="mongodb+srv://kdelac14:SuSYJcSEZwL9OSx4@cluster0.dln0xzp.mongodb.net/media?retryWrites=true&w=majority";
-export const HostName = "MongoDB Atlas";
-export const Secret = "someSecret";
